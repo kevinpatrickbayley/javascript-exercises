81ce99df371efadb89f94c4496293d70b0f5e243
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-09-29 16:07:13 -0700

    Add function that sums ints between 2 poss ints

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    if (typeof x !== 'number' || !Number.isFinite(x) || !Number.isInteger(x) || x < 0){return 'ERROR';} 
+    if (typeof y !== 'number' || !Number.isFinite(y) || !Number.isInteger(y) || y < 0){return 'ERROR';} 
+    
+    let arr = [];
+    arr.push(x);
+    arr.push(y);
+    arr.sort();
+    let sum = 0;
+    for(i=arr[0];i<=arr[1]; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
