7c31f8c183858de9ee8ac6a168ebb268be965206
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-10-14 21:19:59 -0700

    Add fib function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(place) {
+    if (place < 0) return "OOPS";
+    if(place < 1) return 0;
+    if(place <= 2) return 1;
+    let x = 1
+    let y = 1
+    let z = 0;
+    for(let i = 3; i <= place; i++){
+        z = x+y;
+        x = y;
+        y = z;
+    }
+    return z;
 };
 
 // Do not edit below this line
