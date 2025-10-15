828f9016451a3cec2a5090641bef746ac8965d84
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-10-14 20:34:52 -0700

    Add math functions

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accum, curr) => accum + curr,0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accum, curr) => accum * curr,1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	 if (x < 0) {
+    return "Factorial is not defined for negative numbers.";
+  } else if (x === 0 || x === 1) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
 };
 
 // Do not edit below this line
