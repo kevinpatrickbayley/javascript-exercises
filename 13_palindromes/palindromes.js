5b0a958f11a41f1ed9ba473bcaf3ee02d7c82b27
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-10-14 20:57:03 -0700

    Add palindrome checker

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleaned = str
+                    .toLowerCase()
+                    .replace(/[^a-z0-9]+/g, '');
+    const reversed = cleaned
+                        .split('')
+                        .reverse()
+                        .join('');
+    if (cleaned === reversed){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
