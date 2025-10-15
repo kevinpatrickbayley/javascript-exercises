68ee4fc007e10e010c6a6632d76e18383b0ababf
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-10-14 21:56:25 -0700

    Add function to find the oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const now = new Date().getFullYear();
+    const ages = people.map(p => (p.yearOfDeath ?? now) - p.yearOfBirth);
+    const maxValue = Math.max(...ages);
+    const indexOfMaxValue = ages.indexOf(maxValue);
+    return people[indexOfMaxValue];
 };
 
 // Do not edit below this line
